bf200016970d8f33bf19674300f490f0bc8c0c04
hemm <Thomas.Hemmerich.Extern@parcIT.de>
2025-07-22 11:34:17 +0200

    added standalone version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { initFederation } from '@angular-architects/native-federation';
+import { environment } from './environments/environment';
 
 initFederation()
   .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
+  .then(_ => {
+    if (environment.standalone) {
+      return import('./bootstrap-standalone');
+    } else {
+      return import('./bootstrap');
+    }
+  })
   .catch(err => console.error(err));
